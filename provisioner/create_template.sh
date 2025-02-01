12a66174ac549ab5f481e2798d2c31bb669b859f
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-01 14:07:52 +0100

    Update create_template.sh

--- provisioner/create_template.sh
+++ provisioner/create_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Variables communes
-TEMPLATE_DIR="./cloud-version"
+TEMPLATE_DIR="/root/02_Firewall_deployment/cloud_init/cloud-version"
 STORAGE_POOL="local-lvm"
 BRIDGE="vmbr0"
 CORES=2
